e888c05a142c88c14757fbac95036d0e254c25c3
Yujie Wang <jesiner@gmail.com>
2024-04-13 16:41:50 -0700

    add c4 support for use_clm_td!

--- src/Photosynthesis/temperature/clm.jl
+++ src/Photosynthesis/temperature/clm.jl
@@ -1,9 +1,10 @@
 """
 
     use_clm_td!(photo_set::C3ParaSet{FT}, t_opt::FT) where {FT<:AbstractFloat}
+    use_clm_td!(photo_set::C4ParaSet{FT}, t_opt::FT) where {FT<:AbstractFloat}
 
 Update the TD for Vcmax, given
-- `photo_set` `C3ParaSet` type parameter set
+- `photo_set` `C3ParaSet` or `C4ParaSet` type parameter set
 - `t_opt` 10 day average temperature in `[K]`
 
 """
@@ -19,6 +20,17 @@ function use_clm_td!(photo_set::C3ParaSet{FT}, t_opt::FT) where {FT<:AbstractFlo
     jtd.ΔHd_to_R = 200000.0 / GAS_R();
     jtd.ΔSv_to_R = (659.70 - 0.75 * (t_opt - T₀())) / GAS_R();
     jtd.C = 1 + exp( jtd.ΔSv_to_R - jtd.ΔHd_to_R/T₂₅(FT) );
+
+    return nothing
+end
+
+
+function use_clm_td!(photo_set::C4ParaSet{FT}, t_opt::FT) where {FT<:AbstractFloat}
+    vtd = photo_set.VcT;
+    vtd.ΔHa_to_RT25 = 72000.0 / GAS_R() / T₂₅();
+    vtd.ΔHd_to_R = 200000.0 / GAS_R();
+    vtd.ΔSv_to_R = (668.39 - 1.07 * (t_opt - T₀())) / GAS_R();
+    vtd.C = 1 + exp( vtd.ΔSv_to_R - vtd.ΔHd_to_R/T₂₅(FT) );
 
     return nothing
 end
