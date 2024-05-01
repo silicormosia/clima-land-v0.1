0b96d0ff3f8b97ab763ad374cc682d9ffdc98241
Yujie Wang <jesiner@gmail.com>
2024-05-01 09:28:00 -0700

    add RD version GPP and A_GROSS

--- src/SoilPlantAirContinuum/quantities/photosynthesis.jl
+++ src/SoilPlantAirContinuum/quantities/photosynthesis.jl
@@ -2,7 +2,7 @@
 
     A_GROSS(node::SPACMono{FT}) where {FT}
 
-Return the gross photosynthesis rate per layerm per ground area, given
+Return the gross photosynthesis rate per layer per ground area, given
 - `node` [`SPACMono`](@ref) type struct
 
 """
@@ -10,6 +10,20 @@ A_GROSS(node::SPACMono{FT}) where {FT} = [FT[iPS.Ag' * iPS.LAIx * iPS.LA for iPS
                                           FT[iPS.Ac' * iPS.LAIx * iPS.LA for iPS in node.plant_ps],
                                           FT[iPS.Aj' * iPS.LAIx * iPS.LA for iPS in node.plant_ps],
                                           FT[iPS.Ap' * iPS.LAIx * iPS.LA for iPS in node.plant_ps]] ./ node.ga;
+
+
+"""
+
+    A_GROSS_RD(node::SPACMono{FT}) where {FT}
+
+Return the gross photosynthesis rate per layer per ground area, given
+- `node` [`SPACMono`](@ref) type struct
+
+"""
+A_GROSS_RD(node::SPACMono{FT}) where {FT} = [FT[max(0, (iPS.Ag - iPS.ps.Rd)' * iPS.LAIx * iPS.LA) for iPS in node.plant_ps],
+                                             FT[max(0, (iPS.Ac - iPS.ps.Rd)' * iPS.LAIx * iPS.LA) for iPS in node.plant_ps],
+                                             FT[max(0, (iPS.Aj - iPS.ps.Rd)' * iPS.LAIx * iPS.LA) for iPS in node.plant_ps],
+                                             FT[max(0, (iPS.Ap - iPS.ps.Rd)' * iPS.LAIx * iPS.LA) for iPS in node.plant_ps]] ./ node.ga;
 
 
 """
@@ -38,6 +52,22 @@ function GPP(spac::SPACMono{FT}) where {FT<:AbstractFloat}
 
     for iPS in spac.plant_ps
         gpp += numerical∫(iPS.Ag, iPS.LAIx) * iPS.LA;
+    end;
+
+    return gpp / spac.ga
+end;
+
+
+"""
+    GPP_RD(spac::SPACMono{FT}) where {FT<:AbstractFloat}
+
+Return the max(0, GPP - Rd) of the SPAC per ground area
+"""
+function GPP_RD(spac::SPACMono{FT}) where {FT<:AbstractFloat}
+    gpp::FT = 0;
+
+    for iPS in spac.plant_ps
+        gpp += max(0, numerical∫(iPS.An, iPS.LAIx) * iPS.LA);
     end;
 
     return gpp / spac.ga
